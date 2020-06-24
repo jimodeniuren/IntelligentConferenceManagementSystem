39fe80d361d9eabf327fa87f4d558c271e10873c
jimodeniuren <634193463@qq.com>
2020-06-24 16:54:10 +0800

    init some servlet

--- src/servlet/SelectServlet.java
+++ src/servlet/SelectServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
+import dao.AttendenceRecordDao;
 import dao.ConferenceRoomDao;
+import entity.AttendenceRecord;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,10 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "SelectServlet")
 public class SelectServlet extends HttpServlet {
-    private ConferenceRoomDao conferenceRoomDao = new ConferenceRoomDao();
     
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -22,8 +24,23 @@ public class SelectServlet extends HttpServlet {
     }
     
     private void getStatusById(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        ConferenceRoomDao conferenceRoomDao = new ConferenceRoomDao();
         String id = request.getParameter("conference_id");
         request.getSession().setAttribute("select_result",conferenceRoomDao.getStatusById(id));
+        response.sendRedirect("");
+    }
+
+    private void selectByConferenceId(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        AttendenceRecordDao attendenceRecordDao = new AttendenceRecordDao();
+        String id = request.getParameter("conference_id");
+        request.getSession().setAttribute("select_result",attendenceRecordDao.selectById(id,0));
+        response.sendRedirect("");
+    }
+
+    private void selectByParticipantId(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        AttendenceRecordDao attendenceRecordDao = new AttendenceRecordDao();
+        String id = request.getParameter("participant_id");
+        request.getSession().setAttribute("select_result",attendenceRecordDao.selectById(id,1));
         response.sendRedirect("");
     }
 }
