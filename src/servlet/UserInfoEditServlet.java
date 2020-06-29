61fe91cfaf46c75aa5ff5fb99ec962377346b677
lingz_glb <920314957@qq.com>
2020-06-29 19:39:39 +0800

    修改

--- src/servlet/UserInfoEditServlet.java
+++ src/servlet/UserInfoEditServlet.java
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "UserEditServlet")
-public class UserEditServlet extends HttpServlet {
+@WebServlet(name = "UserInfoEditServlet")
+public class UserInfoEditServlet extends HttpServlet {
     private UserDao userDao = new UserDao();
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
